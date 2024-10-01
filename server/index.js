41b0874baa9ecadbc5aca4a94c96899221da4315
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-09-30 23:05:43 -0400

    fix client spelling again on index.js again

--- server/index.js
+++ server/index.js
@@ -49,7 +49,7 @@ app.use(cors({
 
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(express.static("public"));
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../Client/build')));
 
 app.use(passport.initialize());
 app.use(passport.session());
