4a15ca6a9897bf13f9685b3163d2d8c1141f6817
AREZVANI95 <105834594+Servbt@users.noreply.github.com>
2024-10-27 14:58:35 -0400

    add catch all

--- server/index.js
+++ server/index.js
@@ -86,12 +86,6 @@ db.connect();
 // for download routes
 app.use('/api', userRoutes);
 
-// Fallback route: for any unknown route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Client/build', 'index.html'));
-});
-
-
 
 //  solely checks authentication
 app.get('/api/authenticated', (req, res) => {
@@ -288,6 +282,12 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
 });
+
+// Serve React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Client/build', 'index.html'));
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
